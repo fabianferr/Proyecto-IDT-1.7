3ebee38a7f41eb483069e93d112f32d44d4a0f6d
fabian ferreira <fabife801@gmail.com>
2024-02-23 19:59:31 -0300

    proyecto idt

--- backup.sql
+++ backup.sql
@@ -1,2 +1,8 @@
-
-[base de datos]
+USE SQLTestDB;
+GO
+BACKUP DATABASE SQLTestDB
+TO DISK = 'c:\tmp\SQLTestDB.bak'
+WITH FORMAT,
+    MEDIANAME = 'SQLServerBackups',
+    NAME = 'Full Backup of SQLTestDB';
+GO
