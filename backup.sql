25e2481a838fb02ee78833f0f8017fc3fd42aab7
fabian ferreira <fabife801@gmail.com>
2024-02-23 20:00:55 -0300

    proyecto idt

--- backup.sql
+++ backup.sql
@@ -1,7 +1,7 @@
 USE SQLTestDB;
 GO
 BACKUP DATABASE SQLTestDB
-TO DISK = 'c:\tmp\SQLTestDB.bak'
+TO DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup'
 WITH FORMAT,
     MEDIANAME = 'SQLServerBackups',
     NAME = 'Full Backup of SQLTestDB';
